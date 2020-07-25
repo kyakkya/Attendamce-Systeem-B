b37c1b589ae736106d77bbb970b93f0e04fab680
kyakkya <inataaiko123@gmail.com>
2020-07-25 22:57:18 +0000

    user.show追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
-  def new
-  end
+ def show
+   @user = User.find(params[:id]) #VUsersコントローラにリクエストが送信されると、上記のparams[:id]は/users/1の1に置き換わります。
+ end   
+    
+ 
+ def new
+ end
 end
