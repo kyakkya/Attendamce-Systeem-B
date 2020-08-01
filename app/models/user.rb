e42e9d959d0604e1eb281b937f026eab18bfe095
kyakkya <inataaiko123@gmail.com>
2020-08-01 10:22:09 +0000

    7.1終了

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,4 +25,18 @@ class User < ApplicationRecord
    SecureRandom.urlsafe_base64
  end
  
+ # 永続セッションのためハッシュ化したトークンをデータベースに記憶します。
+ def remember
+   self.remember_token = User.new_token
+   update_attribute(:remember_digest, User.digest(remember_token))
+ end  
+  # トークンがダイジェストと一致すればtrueを返します。
+ def authenticated?(remember_token) 
+   BCrypt::Password.new(remember_digest).is_password?(remember_token)
+ end
+
+# ユーザーのログイン情報を破棄します。
+ def forget
+   update_attribute(:remember_digest, nil)
+ end
 end 
