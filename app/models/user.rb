39924ebcd468f7275cbe46e5feb2d128829ac278
kyakkya <inataaiko123@gmail.com>
2020-08-02 08:56:31 +0000

    Finish full-fledged-login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save { self.email = email.downcase }
+ 
   validates :name, presence: true, length: { maximum: 50 }
+ 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 100},
              format: { with: VALID_EMAIL_REGEX },
@@ -27,12 +30,15 @@ class User < ApplicationRecord
  
  # 永続セッションのためハッシュ化したトークンをデータベースに記憶します。
  def remember
-   self.remember_token = User.new_token
-   update_attribute(:remember_digest, User.digest(remember_token))
+  self.remember_token = User.new_token
+  update_attribute(:remember_digest, User.digest(remember_token))
  end  
+ 
   # トークンがダイジェストと一致すればtrueを返します。
- def authenticated?(remember_token) 
-   BCrypt::Password.new(remember_digest).is_password?(remember_token)
+ def authenticated?(remember_token)
+  # ダイジェストが存在しない場合はfalseを返して終了します。
+  return false if remember_digest.nil?
+  BCrypt::Password.new(remember_digest).is_password?(remember_token)
  end
 
 # ユーザーのログイン情報を破棄します。
